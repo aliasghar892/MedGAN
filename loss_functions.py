f24ee8cf8c8634dc82f04ddfd41240aec12af07c
aliasghar <aliasghar.krm5@gmail.com>
2024-11-17 21:07:07 +0330

    fixed cgan loss based on gan and pix2pix implementations , also pytorch l1loss replaced

--- loss_functions.py
+++ loss_functions.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn import L1Loss, BCELoss
 
 # *******************************************************
 # NOTE : y = generated , x = ground truth translated data
@@ -51,33 +52,43 @@ def gram_matrix(tensor):
     gram = torch.mm(tensor, tensor.t())
     return gram/(h*w*d)
 
-# review custom l1loss
-
 
 class CGANGeneratorLoss(nn.Module):
     def __init__(self):
         super(CGANGeneratorLoss, self).__init__()
+        self.bce = BCELoss()
+        self.l1 = L1Loss()
 
-    def forward(self, y_patch):
-        return torch.mean(-1 * torch.log10(y_patch))
+    def forward(self, y_patch, x, y):
+        # return torch.mean(-1 * torch.log10(y_patch))
+        valid = torch.ones_like(y_patch)
+        loss = self.bce(y_patch, valid) + self.alpha * self.l1(y, x)
+        return loss
 
 
 class CGANDiscreminatorLoss(nn.Module):
     def __init__(self):
         super(CGANDiscreminatorLoss, self).__init__()
+        self.bce = BCELoss()
 
     def forward(self, x, y, x_patch, y_patch, lambdaL1):
-        # review , -1 applied in the logs
-        cgan_loss = torch.mean(-1*torch.log10(x_patch)) + \
-            torch.mean(-1*torch.log10(1-y_patch))
-        # l1_loss = lambdaL1 * L1Loss(x, y, reduction="mean")
-        l1_loss = lambdaL1 * (torch.mean(torch.abs(x-y)))
-        return cgan_loss+l1_loss
+        # cgan_loss = torch.mean(-1*torch.log10(x_patch)) + \
+        #     torch.mean(-1*torch.log10(1-y_patch))
+        # # l1_loss = lambdaL1 * L1Loss(x, y, reduction="mean")
+        # l1_loss = lambdaL1 * (torch.mean(torch.abs(x-y)))
+        # return cgan_loss+l1_loss
+        valid = torch.ones_like(y_patch)
+        fake = torch.zeros_like(y_patch)
+        real_loss = self.bce(x_patch, valid)
+        fake_loss = self.bce(y_patch, fake)
+        loss = (real_loss + fake_loss) / 2
+        return loss
 
 
 class PerceptualLoss(nn.Module):
     def __init__(self):
         super(PerceptualLoss, self).__init__()
+        self.l1 = L1Loss()
 
     def forward(self, x_fmaps, y_fmaps, lambda0, lambda_perceptual_layer):
         ploss = 0
@@ -88,16 +99,18 @@ class PerceptualLoss(nn.Module):
             d = x_fmaps[0][i].shape[0]
             h = x_fmaps[0][i].shape[1]
             w = x_fmaps[0][i].shape[2]
-            # p0 = (lambdaP0/(d*h*w)) * L1Loss(x_fmaps[0][i], y_fmaps[0][i])
             p0 = (lambda_perceptual_layer[0]/(d*h*w)) * \
-                torch.mean(torch.abs(x_fmaps[0][i] - y_fmaps[0][i]))
+                self.l1(x_fmaps[0][i], y_fmaps[0][i])
+            # p0 = (lambda_perceptual_layer[0]/(d*h*w)) * \
+            #     torch.mean(torch.abs(x_fmaps[0][i] - y_fmaps[0][i]))
         for i in range(len(x_fmaps[1])):
             d = x_fmaps[1][i].shape[1]
             h = x_fmaps[1][i].shape[1]
             w = x_fmaps[1][i].shape[2]
-            # p1 = (lambdaP1/(d*h*w)) * L1Loss(x_fmaps[1][i], y_fmaps[1][i])
             p1 = (lambda_perceptual_layer[1]/(d*h*w)) * \
-                torch.mean(torch.abs(x_fmaps[1][i] - y_fmaps[1][i]))
+                self.l1(x_fmaps[1][i], y_fmaps[1][i])
+            # p1 = (lambda_perceptual_layer[1]/(d*h*w)) * \
+            #     torch.mean(torch.abs(x_fmaps[1][i] - y_fmaps[1][i]))
         ploss = lambda0*(p0+p1)
         return (ploss)
 
